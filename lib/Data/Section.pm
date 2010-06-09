886e99950f26b66ccde859aea2eb9ba121069d14
Christian Walde <walde.christian@googlemail.com>
2010-06-10 00:50:17 +0200

    added some documentation in the default header regex

--- lib/Data/Section.pm
+++ lib/Data/Section.pm
@@ -157,7 +157,19 @@ C<local_section_data> method.
 sub _mk_reader_group {
   my ($mixin, $name, $arg, $col) = @_;
   my $base = $col->{INIT}{into};
-  my $header_re = $arg->{header_re} || qr/\A_+\[\s*([^\]]+?)\s*\]_+\r?\Z/;
+  
+  my $default_header_re = qr/
+    \A                (?# start )
+      _+\[            (?# __[ )
+        \s*           (?# any whitespace )
+          ([^\]]+?)   (?# this is the actual name of the section )
+        \s*           (?# any whitespace )
+      \]_+            (?# ]__ )
+      \r?             (?# possible cariage return for windows files )
+    \Z                (?# end )
+  /x;
+  
+  my $header_re = $arg->{header_re} || $default_header_re;
   $arg->{inherit} = 1 unless exists $arg->{inherit};
 
   my %export;
